c949346059509cc260ba675d1a88f9d0a2b9fd69
LunkSnee <LunkSnee@users.noreply.github.com>
2023-02-05 15:07:25 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Entrypoint for idrive
+#/work/IDriveForLinux/scripts/check_for_update.pl silent
 
-/work/IDriveForLinux/scripts/check_for_update.pl silent
 service idrivecron start
 
 # Keep container up
